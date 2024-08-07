cee5e07eae0d3952c2717a9841176c9414c2d9fc
Wayne <wayneotemahegesa@gmail.com>
2024-08-07 08:53:14 +0300

    added to middlew

--- MainApp/settings.py
+++ MainApp/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 # CORS Headers settings
